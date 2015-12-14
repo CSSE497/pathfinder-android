3ed9457e3fab50c45623baefcf27abc4268d511b
robinsdm <robinsdm@rose-hulman.edu>
2015-12-14 02:37:47 -0500

    Added a mini test server

--- src/test/java/xyz/thepathfinder/PathfinderTest.java
+++ src/test/java/xyz/thepathfinder/PathfinderTest.java
@@ -1,10 +1,13 @@
 package xyz.thepathfinder;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import xyz.thepathfinder.android.Cluster;
 import xyz.thepathfinder.android.Pathfinder;
-import xyz.thepathfinder.android.SubscribableCrudModel;
-import xyz.thepathfinder.android.TransportStatus;
 
 import javax.websocket.DeploymentException;
 import java.io.IOException;
@@ -15,6 +18,20 @@ import static org.junit.Assert.assertTrue;
 
 public class PathfinderTest {
 
+    private TestServer server;
+    private TestMessager messager;
+
+    @Before
+    public void setupSever() {
+        this.server = new TestServer();
+        this.messager = TestEndpoint.getMessager();
+    }
+
+    @After
+    public void stopServer() {
+        this.server.stopServer();
+    }
+
     public void waitForMessages(Pathfinder pathfinder, int messageCount) throws InterruptedException {
         while(pathfinder.getReceivedMessageCount() != messageCount) {
             Thread.sleep(10);
@@ -23,12 +40,42 @@ public class PathfinderTest {
 
     @Test(timeout = 10000)
     public void testConnection() throws URISyntaxException, IOException, DeploymentException, InterruptedException {
-        URI url = new URI("ws://api.thepathfinder.xyz:9000/socket");
+        URI url = new URI("ws://localhost:8025/socket");
         Pathfinder pathfinder = new Pathfinder("9c4166bb-9535-49e1-8844-1904a0b1f45b", "", url);
         assertTrue(pathfinder.isConnected());
         Cluster cluster = pathfinder.getDefaultCluster();
+
+        JsonObject receive = new JsonObject();
+        receive.addProperty("model", "Cluster");
+        receive.addProperty("path", "/default");
+        receive.addProperty("type", "read");
+
+        this.messager.setReceive(receive.toString());
+
+        JsonObject send = new JsonObject();
+        send.addProperty("type", "model");
+        send.addProperty("path", "/default");
+        send.addProperty("model", "Cluster");
+
+        JsonObject value = new JsonObject();
+        value.addProperty("path", "/default");
+        value.addProperty("model", "Cluster");
+
+        JsonArray transports = new JsonArray();
+        JsonArray commodities = new JsonArray();
+        JsonArray subclusters = new JsonArray();
+
+        value.add("transports", transports);
+        value.add("commodities", commodities);
+        value.add("subClusters", subclusters);
+
+        send.add("value", value);
+
+        this.messager.setSend(send.toString());
+
         cluster.connect();
-        //this.waitForMessages(pathfinder, 1);
+        this.waitForMessages(pathfinder, 1);
+        Assert.assertTrue(this.messager.getCorrect());
         cluster.subscribe();
         //this.waitForMessages(pathfinder, 3);
         //SubscribableCrudModel transport = cluster.createTransport("hi", 32.32,42, TransportStatus.OFFLINE,null);
---
+++ src/test/java/xyz/thepathfinder/TestMessager.java
@@ -0,0 +1,33 @@
+package xyz.thepathfinder;
+
+public class TestMessager {
+
+    public String receive;
+    public String send;
+    public boolean correct;
+
+    public void setReceive(String message) {
+        this.receive = message;
+    }
+
+    public void setSend(String message) {
+        this.send = message;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public String getReceive() {
+        return this.receive;
+    }
+
+    public String getSend() {
+        return this.send;
+    }
+
+    public boolean getCorrect() {
+        return this.correct;
+    }
+
+}
